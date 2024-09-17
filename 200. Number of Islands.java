ae18ed132d0ad15c4aa62863c4ec9d5643f8aa80
Nikhil Negi <157276223+neginikhil@users.noreply.github.com>
2024-09-17 16:46:59 +0530

    DFS and BFS

--- 200. Number of Islands.java
+++ 200. Number of Islands.java
@@ -1,3 +1,5 @@
+// DFS Solution
+
 class Solution {
     public void dfs(char[][] grid, int i, int j){
         if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j] != '1'){
@@ -22,6 +24,49 @@ class Solution {
                 if(grid[i][j] == '1'){
                     cnt++;
                     dfs(grid,i,j);
+                }
+            }
+        }
+        return cnt;
+    }
+}
+
+
+
+
+// BFS Solution
+
+class Solution {
+    public int numIslands(char[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        if(grid == null || rows == 0){
+            return 0;
+        }
+        int cnt = 0;
+        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        Queue<int[]> q = new LinkedList<>();
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j] == '1'){
+                    cnt++;
+                    q.add(new int[]{i,j});
+                    while(!q.isEmpty()){
+                        int[] temp = q.poll();
+                        int x = temp[0];
+                        int y = temp[1];
+                        if(x<0 || x>=rows || y<0 || y>=cols || grid[x][y] == '0'){
+                            continue;
+                        }
+                        grid[x][y] = '0';
+                        for(int[] dir : directions){
+                            int dx = x + dir[0];
+                            int dy = y + dir[1];
+                            if(dx>=0 || dx<rows || dy>=0 || dy<cols || grid[x][y] == '1'){
+                                q.add(new int[]{dx,dy});
+                            }
+                        }
+                    }
                 }
             }
         }
